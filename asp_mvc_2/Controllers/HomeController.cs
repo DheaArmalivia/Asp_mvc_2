005620b3d94fc034e2fe34f7f173ff9bf21a729e
Dhea Armalivia <daffyderr@gmail.com>
2017-02-12 20:05:47 +0700

    tugas 3 modul 2

--- asp_mvc_2/Controllers/HomeController.cs
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using asp_mvc_2.Security;
 namespace asp_mvc_2.Controllers
 {
     public class HomeController : Controller
     {
-         public ActionResult Index() {
-             return View();
-         }
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-         [Authorize]
-         public ActionResult Welcome()
-         {
-             return View();
-         }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            {
+                return View();
+            }
+        }
+
+
     }
 }
